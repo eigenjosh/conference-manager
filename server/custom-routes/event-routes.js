4b47e5821b2ca18f3108bf8d19a2e9333c0794e8
Kyle Krawl <kyle.krawl@gmail.com>
2017-12-07 14:33:32 -0700

    Added event routes.

---
+++ server/custom-routes/event-routes.js
@@ -0,0 +1,29 @@
+let Events = require('../models/event')
+let Activities = require('../models/activity')
+
+module.exports = {
+    getActivitiesByEventId: {
+        path: '/events/:eventId/activities',
+        reqType: 'get',
+        method(req, res, next) {
+            let action = 'Find Activities By Event'
+            Activities.find({ eventId: req.params.eventId })
+                .then(activities => {
+                    res.send(handleResponse(action, activities))
+                }).catch(error => {
+                    return next(handleResponse(action, null, error))
+                })
+        }
+    }
+}
+
+function handleResponse(action, data, error) {
+    var response = {
+        action: action,
+        data: data
+    }
+    if (error) {
+        response.error = error
+    }
+    return response
+}
