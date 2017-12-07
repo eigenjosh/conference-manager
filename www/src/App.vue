002ce7ef6169fc2da92d26f685d4839c8b6a8d2e
Kaci Parker <kaci.joy@hotmail.com>
2017-12-07 12:19:24 -0700

    Added Home Styling

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <!-- <error></error> -->
     <router-view></router-view>
+    <!-- <error></error> -->
   </div>
 </template>
 
@@ -22,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
--- www/src/components/Home.vue
+++ www/src/components/Home.vue
@@ -11,57 +11,58 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Brand</a>
+                    <a class="navbar-brand">Confer</a>
                 </div>
 
-                <!-- Collect the nav links, forms, and other content for toggling -->
+                <!-- MENU DROWDOWN -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="active">
-                            <a href="#">Link
-                                <span class="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">Link</a>
-                        </li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">MENU
                                 <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a href="#">Action</a>
+                                    <button type="button" class="btn navbar-btn">Find Events</button>
                                 </li>
                                 <li>
-                                    <a href="#">Another action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Something else here</a>
+                                    <button type="button" class="btn navbar-btn">Event Schedule</button>
                                 </li>
                                 <li role="separator" class="divider"></li>
                                 <li>
-                                    <a href="#">Separated link</a>
+                                    <button type="button" class="btn navbar-btn">My Schedule</button>
+                                </li>
+                                <li>
+                                    <button type="button" class="btn navbar-btn">My Notes</button>
                                 </li>
                                 <li role="separator" class="divider"></li>
                                 <li>
-                                    <a href="#">One more separated link</a>
+                                    <button type="button" class="btn navbar-btn">Login</button>
+                                </li>
+                                <li>
+                                    <button type="button" class="btn navbar-btn">Logout</button>
                                 </li>
                             </ul>
                         </li>
                     </ul>
+                    <!-- SEARCH BAR -->
                     <form class="navbar-form navbar-left">
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Search">
                         </div>
+                        <!-- SUBMIT BUTTON -->
                         <button type="submit" class="btn btn-default">Submit</button>
                     </form>
                     <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#">Link</a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown
+                        <!-- LOGIN BUTTON -->
+                        <button type="button" class="btn btn-primary navbar-btn">Login</button>
+                        <!-- SIGN UP BUTTON -->
+                        <button type="button" class="btn btn-default navbar-btn">Sign-Up</button>
+
+                        <!-- SECOND DROPDOWN ON RIGHT -->
+
+                        <!-- <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown
                                 <span class="caret"></span>
                             </a>
                             <ul class="dropdown-menu">
@@ -79,14 +80,20 @@
                                     <a href="#">Separated link</a>
                                 </li>
                             </ul>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
                 <!-- /.navbar-collapse -->
             </div>
             <!-- /.container-fluid -->
         </nav>
-
+        <div class="jumbotron main-pic">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h1>Welcome to Confer: A Conference Manager Application designed to dynamically handle schedules for events.</h1>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -103,4 +110,37 @@
 </script>
 
 <style>
+    /* .main-pic {
+        background-image: url('http://www.memorymakersevents.com/my_uploads/2017/10/conferencea-.jpg')
+    } */
+
+    /* @media (max-width: 2000px) {
+        .navbar-header {
+            float: none;
+        }
+        .navbar-toggle {
+            display: block;
+        }
+        .navbar-collapse {
+            border-top: 1px solid transparent;
+            box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.1);
+        }
+        .navbar-collapse.collapse {
+            display: none!important;
+        }
+        .navbar-collapse.collapse.in {
+            display: block!important;
+        }
+        .navbar-nav {
+            float: none!important;
+            margin: 7.5px -15px;
+        }
+        .navbar-nav>li {
+            float: none;
+        }
+        .navbar-nav>li>a {
+            padding-top: 10px;
+            padding-bottom: 10px;
+        }
+    } */
 </style>
