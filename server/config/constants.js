4716ab0f5076e41a2373b1051b10592b019c0667
Brandon Gulley <brandongulley@Brandons-MacBook-Pro.local>
2017-12-07 09:15:52 -0700

    initalize

---
+++ server/config/constants.js
@@ -0,0 +1,30 @@
+const actions = {
+  create: 'Create',
+  update: 'Update',
+  remove: 'Remove',
+  find: 'Find',
+  findAll: 'Find All'
+}
+
+const models = {
+  board: {
+    name: 'Board',
+    endpoint: 'boards'
+  },
+  list: {
+    name: 'List',
+    endpoint: 'lists'
+  },
+  user: {
+    name: 'User',
+    endpoint: 'users',
+    preventDefaultApi: true,
+    useCustomRoutes: true
+  }
+}
+
+
+module.exports = {
+  actions,
+  models
+}
---
+++ www/src/components/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="error.message"> 
+    Error: {{error}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error',
+  computed:{
+    error(){
+      return this.$store.state.error
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
