f8f75bbbf5b3543403f9059e8e5227c12bcef781
Kaci Parker <kaci.joy@hotmail.com>
2017-12-07 15:59:05 -0700

    Added in MySchedule.vue. Finished hooking up login and sign up buttons.

---
+++ www/src/components/MySchedule.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>MY SCHEDULE</h1>
+            </div>
+        </div>
+    </div>
+</template>
