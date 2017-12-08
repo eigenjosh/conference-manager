03aa71e644ecdc43f0cef8b55f1e5ff7ed7d41df
Kaci Parker <kaci.joy@hotmail.com>
2017-12-08 16:43:20 -0700

    Added in note modals.

--- www/src/components/Notes.vue
+++ www/src/components/Notes.vue
@@ -83,7 +83,7 @@
             </div>
         </div>
 
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-xs-12">
                     <h2 class="text-center">
@@ -92,11 +92,32 @@
                 </div>
             </div>
             <div v-for="userNote in userNotes" class="row well">
+                <button type="button" class="btn btn-primary navbar-btn" data-toggle="modal" data-target="#myModal2">
                     <div class="col-xs-12">
-                            <h1>{{userNote.title}}</h1>
-                        </div>
-                <div class="col-xs-12">
+                        <h1>{{userNote.title}}</h1>
+                    </div>
+                </button>
+                <!-- <div class="col-xs-12">
                     <h4>{{userNote.body}}</h4>
+                </div> -->
+            </div>
+        </div>
+
+        <div v-for="userNote in userNotes" class="modal-notes">
+            <div id="myModal2" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4>{{userNote.title}}</h4>
+                            <p>{{userNote.body}}</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
