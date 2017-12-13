319a2545083385d4f70906cea5b92d6059d22560
Josh Eigen <josh.eigen@gmail.com>
2017-12-13 15:46:35 -0700

    updated dev server

--- server/config/dev-server.js
+++ server/config/dev-server.js
@@ -41,14 +41,48 @@ app.use(Validate)
 app.use('/api', api)
 app.use('/', defaultErrorHandler)
 
+//region ****** CAREFUL SOCKETS BELOW **********
 let io = require('socket.io')(server, {
     origins: '*:*'
 })
 
+
 let connectedUsers = {}
+let rooms = {
+    general: {
+        name: 'General',
+        users: {},
+        moderators: {},
+        banned: {}
+    }
+}
+
+function Room(name, client) {
+    this.name = name
+    this.users = {}
+    this.moderators = {}
+    this.moderators[client.userId] = client
+    this.users[client.userId] = client
+    this.banned = {}
+    rooms[name.toLowerCase()] = this
+}
+
+function joinRoom(socket, roomName, client) {
+    var room = rooms[roomName.toLowerCase()] || new Room(roomName, client)
+    socket.join(room.name)
+    room.users[client.userId] = client
+    socket.to(room.name).emit('userJoined', {
+        userId: client.userId,
+        name: client.name
+    })
+    socket.emit('joinedRoom', room)
+    return room
+}
 
 io.on('connection', function (socket) {
     var client = {}
+    var activeRoom = {}
+
     socket.emit('CONNECTED', {
         socket: socket.id,
         message: 'Welcome to the Jungle',
@@ -56,27 +90,39 @@ io.on('connection', function (socket) {
     })
 
     socket.on('setUser', (user) => {
-        client = user
-        connectedUsers[user._id] = {
+        client = {
             userId: user._id,
             name: user.name
         }
-        socket.broadcast.emit('userConnected', {
-            userId: user._id,
-            name: user.name
-        })
-        console.log(connectedUsers)
+        connectedUsers[user._id] = client
+        joinRoom(socket, client.userId, client)
+        activeRoom = joinRoom(socket, rooms.general.name, client)
+        socket.broadcast.emit('userConnected', client)
     })
 
     socket.on('update', payload => {
-        socket.broadcast.emit('receiveUpdate', payload)
+        socket.to(activeRoom.name).emit('receiveUpdate', payload)
+    })
+
+    socket.on('joinRoom', roomName => {
+        activeRoom = joinRoom(socket, roomName, client)
+    })
+
+    socket.on('leaveRoom', roomName => {
+        if (roomName.toLowerCase() == 'general' || roomName == client.userId) { return }
+        socket.leave(roomName)
+        socket.to(roomName).emit('userLeft', client)
+        socket.emit('leftRoom', rooms.general)
     })
 
     socket.on('disconnect', () => {
         delete connectedUsers[client._id]
+        //CLEANUP ROOM USERs
         socket.broadcast.emit('userDisconnected', client._id)
     })
 
 })
+
+//endregion
 
 module.exports = server
